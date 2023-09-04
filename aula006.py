4cb927f28d6e92724d8d1afec05a7a0d47bdc1d4
Bianca-karoline <biancakarolineferreira14@gmail.com>
2023-09-04 12:51:49 -0300

    aula 6 a 10

---
+++ aula006.py
@@ -0,0 +1,5 @@
+# conversão de tipos, coerção
+# type convertion, typecasting, coercion
+print(int('1'), type(int('1')))
+print(type(float('1') + 1))
+print(bool(' '))
---
+++ aula007.py
@@ -0,0 +1,5 @@
+nome = 'Bianca'
+idade = 20
+maior_de_idade = idade >= 18
+print(f'nome ={nome} {idade=}')
+print(f'É maior? {maior_de_idade}')
