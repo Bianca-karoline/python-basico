803489ef2f4e4fd59d50505ea2f871ee7c348938
Bianca-karoline <biancakarolineferreira14@gmail.com>
2023-09-06 14:18:35 -0300

    aulas 16 a 25

---
+++ aula024.py
@@ -0,0 +1,17 @@
+nome = 'Bianca'
+print(nome[2])
+print(nome[-4])
+print('nca' in nome)
+print('zero' in nome)
+print(10 * '-')
+print('nca' not in nome)
+print('zero' not in nome)
+
+nome = input('Digite seu nome: ')
+encontrar = input('Digite o que deseja encontrar: ')
+
+if encontrar in nome:
+    print(f'{encontrar} está em {nome}')
+else:
+    print(f'{encontrar} não está em {nome}')
+    
